1e4cbdcdd4c7d4b39a24624a35ef8561105a82d0
Jahnavi Kotapati <149530397+jahnavikotapati@users.noreply.github.com>
2025-07-08 12:00:18 -0500

    Time: 8 ms (49.72%) | Memory: 64.4 MB (58.13%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,12 @@
+function isPalindrome(x) {
+    
+    if (x < 0) return false;
+
+    
+    const str = x.toString();
+
+    
+    const reversedStr = str.split('').reverse().join('');
+    
+    return str === reversedStr;
+}
