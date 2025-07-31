e1ac3da2562bf82b1eb1cc1ee18148212e41e006
Jahnavi Kotapati <149530397+jahnavikotapati@users.noreply.github.com>
2025-07-31 00:30:12 -0500

    Time: 213 ms (33.33%) | Memory: 85.6 MB (33.33%) - LeetSync

---
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.js
@@ -0,0 +1,18 @@
+var subarrayBitwiseORs = function(arr) {
+    let result = new Set();
+    let prev = new Set();
+
+    for (let num of arr) {
+        let curr = new Set();
+        curr.add(num);
+        for (let val of prev) {
+            curr.add(val | num);
+        }
+        for (let val of curr) {
+            result.add(val);
+        }
+        prev = curr;
+    }
+
+    return result.size;
+};
