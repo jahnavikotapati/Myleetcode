4ab75a2a6a8d57d1ed136899029ce5b3611da76b
Jahnavi Kotapati <149530397+jahnavikotapati@users.noreply.github.com>
2025-08-13 19:25:39 -0500

    Time: 1 ms (81.82%) | Memory: 55.9 MB (34.09%) - LeetSync

---
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestGoodInteger = function(num) {
+    let maxGood = "";
+
+    for (let i = 0; i <= num.length - 3; i++) {
+        let sub = num.slice(i, i + 3);
+        if (sub[0] === sub[1] && sub[1] === sub[2]) {
+            if (sub > maxGood) {
+                maxGood = sub;
+            }
+        }
+    }
+    return maxGood;
+};
