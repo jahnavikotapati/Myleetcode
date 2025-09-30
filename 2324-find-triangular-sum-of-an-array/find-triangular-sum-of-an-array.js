357e6816faba0724bd5b075b631fd72198d9dc29
Jahnavi Kotapati <149530397+jahnavikotapati@users.noreply.github.com>
2025-09-29 21:00:20 -0500

    Time: 106 ms (43.33%) | Memory: 60.4 MB (41.67%) - LeetSync

---
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.js
@@ -0,0 +1,10 @@
+var triangularSum = function(nums) {
+    while (nums.length > 1) {
+        let temp = []
+        for (let i = 0; i < nums.length - 1; i++) {
+            temp.push((nums[i] + nums[i + 1]) % 10)
+        }
+        nums = temp
+    }
+    return nums[0]
+};
