f97f91a6479a74d27af89be9b352116e687d9928
Jahnavi Kotapati <149530397+jahnavikotapati@users.noreply.github.com>
2025-11-10 18:52:17 -0600

    Time: 63 ms (72.38%) | Memory: 58.3 MB (65.71%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.js
@@ -0,0 +1,16 @@
+var findMaxForm = function(strs, m, n) {
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0))
+    for (let str of strs) {
+        let zeros = 0, ones = 0
+        for (let ch of str) {
+            if (ch === '0') zeros++
+            else ones++
+        }
+        for (let i = m; i >= zeros; i--) {
+            for (let j = n; j >= ones; j--) {
+                dp[i][j] = Math.max(dp[i][j], dp[i - zeros][j - ones] + 1)
+            }
+        }
+    }
+    return dp[m][n]
+}
