3eb5229c7043591d3794fb0829050226131e33fa
Jahnavi Kotapati <149530397+jahnavikotapati@users.noreply.github.com>
2026-01-02 19:00:52 -0600

    Time: 2 ms (85.00%) | Memory: 54.8 MB (90.00%) - LeetSync

---
+++ 1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.js
@@ -0,0 +1,12 @@
+var numOfWays = function(n) {
+    const MOD = 1000000007;
+    let aba = 6;
+    let abc = 6;
+    for (let i = 2; i <= n; i++) {
+        const newAba = (aba * 3 + abc * 2) % MOD;
+        const newAbc = (aba * 2 + abc * 2) % MOD;
+        aba = newAba;
+        abc = newAbc;
+    }
+    return (aba + abc) % MOD;
+};
