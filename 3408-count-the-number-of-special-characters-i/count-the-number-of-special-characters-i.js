ef7cfbecbfb4905bd4f352e0d3c6086b86225a10
Jahnavi Kotapati <149530397+jahnavikotapati@users.noreply.github.com>
2026-05-25 23:31:25 -0500

    Time: 2 ms (52.38%) | Memory: 55.9 MB (69.84%) - LeetSync

---
+++ 3408-count-the-number-of-special-characters-i/count-the-number-of-special-characters-i.js
@@ -0,0 +1,22 @@
+var numberOfSpecialChars = function(word) {
+    let lower = new Set();
+    let upper = new Set();
+
+    for (let ch of word) {
+        if (ch >= 'a' && ch <= 'z') {
+            lower.add(ch);
+        } else {
+            upper.add(ch.toLowerCase());
+        }
+    }
+
+    let count = 0;
+
+    for (let ch of lower) {
+        if (upper.has(ch)) {
+            count++;
+        }
+    }
+
+    return count;
+};
